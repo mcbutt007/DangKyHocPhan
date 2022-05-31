2ff31f30bfb5a51f7bb864d5a5eb3ca4cca209c6
minhlong149 <minhlong9514@gmail.com>
2022-05-31 17:29:56 +0700

    Test chức năng

--- DangKyHocPhan/ChuaHTHP.Designer.cs
+++ DangKyHocPhan/ChuaHTHP.Designer.cs
@@ -31,20 +31,12 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.title = new System.Windows.Forms.Label();
             this.grBoxThongTin = new System.Windows.Forms.GroupBox();
-            this.comboBoxNamHoc = new System.Windows.Forms.ComboBox();
-            this.btnTruyVan = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox_HocKy = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnThoat = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
             this.dgvChuaHP = new System.Windows.Forms.DataGridView();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.cboHocKy = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.grBoxThongTin.SuspendLayout();
             this.panel2.SuspendLayout();
@@ -57,17 +49,19 @@
             this.panel1.Controls.Add(this.title);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(628, 51);
+            this.panel1.Size = new System.Drawing.Size(837, 63);
             this.panel1.TabIndex = 0;
             // 
             // title
             // 
             this.title.AutoSize = true;
             this.title.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.title.Location = new System.Drawing.Point(42, 19);
+            this.title.Location = new System.Drawing.Point(56, 23);
+            this.title.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(519, 19);
+            this.title.Size = new System.Drawing.Size(637, 24);
             this.title.TabIndex = 0;
             this.title.Text = "DANH SÁCH SINH VIÊN CHƯA HOÀN THÀNH VIỆC ĐÓNG HỌC PHÍ";
             this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -75,94 +69,29 @@
             // 
             // grBoxThongTin
             // 
-            this.grBoxThongTin.Controls.Add(this.comboBoxNamHoc);
-            this.grBoxThongTin.Controls.Add(this.btnTruyVan);
-            this.grBoxThongTin.Controls.Add(this.label2);
-            this.grBoxThongTin.Controls.Add(this.comboBox_HocKy);
-            this.grBoxThongTin.Controls.Add(this.label1);
+            this.grBoxThongTin.Controls.Add(this.cboHocKy);
+            this.grBoxThongTin.Controls.Add(this.label3);
             this.grBoxThongTin.Dock = System.Windows.Forms.DockStyle.Top;
             this.grBoxThongTin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grBoxThongTin.Location = new System.Drawing.Point(0, 51);
+            this.grBoxThongTin.Location = new System.Drawing.Point(0, 63);
+            this.grBoxThongTin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grBoxThongTin.Name = "grBoxThongTin";
-            this.grBoxThongTin.Size = new System.Drawing.Size(628, 75);
+            this.grBoxThongTin.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grBoxThongTin.Size = new System.Drawing.Size(837, 92);
             this.grBoxThongTin.TabIndex = 1;
             this.grBoxThongTin.TabStop = false;
             this.grBoxThongTin.Text = "Thông tin năm học";
             this.grBoxThongTin.Enter += new System.EventHandler(this.groupBox1_Enter);
-            // 
-            // comboBoxNamHoc
-            // 
-            this.comboBoxNamHoc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxNamHoc.FormattingEnabled = true;
-            this.comboBoxNamHoc.Items.AddRange(new object[] {
-            "2022-2023",
-            "2021-2022",
-            "2020-2021",
-            "2019-2020",
-            "2018-2019",
-            "2017-2018",
-            "2016-2017",
-            "2015-2016",
-            "2014-2015"});
-            this.comboBoxNamHoc.Location = new System.Drawing.Point(99, 47);
-            this.comboBoxNamHoc.Name = "comboBoxNamHoc";
-            this.comboBoxNamHoc.Size = new System.Drawing.Size(151, 26);
-            this.comboBoxNamHoc.TabIndex = 3;
-            // 
-            // btnTruyVan
-            // 
-            this.btnTruyVan.BackColor = System.Drawing.Color.RoyalBlue;
-            this.btnTruyVan.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnTruyVan.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnTruyVan.Location = new System.Drawing.Point(530, 38);
-            this.btnTruyVan.Name = "btnTruyVan";
-            this.btnTruyVan.Size = new System.Drawing.Size(75, 35);
-            this.btnTruyVan.TabIndex = 4;
-            this.btnTruyVan.Text = "Tìm";
-            this.btnTruyVan.UseVisualStyleBackColor = false;
-            this.btnTruyVan.Click += new System.EventHandler(this.btnTruyVan_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(24, 47);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 18);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Năm học";
-            // 
-            // comboBox_HocKy
-            // 
-            this.comboBox_HocKy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox_HocKy.FormattingEnabled = true;
-            this.comboBox_HocKy.Items.AddRange(new object[] {
-            "1",
-            "2"});
-            this.comboBox_HocKy.Location = new System.Drawing.Point(99, 18);
-            this.comboBox_HocKy.Name = "comboBox_HocKy";
-            this.comboBox_HocKy.Size = new System.Drawing.Size(57, 26);
-            this.comboBox_HocKy.TabIndex = 1;
-            this.comboBox_HocKy.SelectedIndexChanged += new System.EventHandler(this.comboBox_HocKy_SelectedIndexChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(24, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 18);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Học kỳ";
             // 
             // btnThoat
             // 
             this.btnThoat.BackColor = System.Drawing.Color.Red;
             this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThoat.ForeColor = System.Drawing.SystemColors.Menu;
-            this.btnThoat.Location = new System.Drawing.Point(530, 3);
+            this.btnThoat.Location = new System.Drawing.Point(706, 4);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(75, 34);
+            this.btnThoat.Size = new System.Drawing.Size(100, 42);
             this.btnThoat.TabIndex = 0;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = false;
@@ -172,9 +101,10 @@
             // 
             this.panel2.Controls.Add(this.dgvChuaHP);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 126);
+            this.panel2.Location = new System.Drawing.Point(0, 155);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(628, 214);
+            this.panel2.Size = new System.Drawing.Size(837, 264);
             this.panel2.TabIndex = 3;
             // 
             // dgvChuaHP
@@ -182,81 +112,59 @@
             this.dgvChuaHP.AllowUserToAddRows = false;
             this.dgvChuaHP.AllowUserToDeleteRows = false;
             this.dgvChuaHP.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvChuaHP.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column5,
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4});
             this.dgvChuaHP.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvChuaHP.Location = new System.Drawing.Point(0, 0);
-            this.dgvChuaHP.Margin = new System.Windows.Forms.Padding(100);
+            this.dgvChuaHP.Margin = new System.Windows.Forms.Padding(133, 123, 133, 123);
             this.dgvChuaHP.Name = "dgvChuaHP";
             this.dgvChuaHP.ReadOnly = true;
-            this.dgvChuaHP.Size = new System.Drawing.Size(628, 214);
+            this.dgvChuaHP.RowHeadersWidth = 51;
+            this.dgvChuaHP.Size = new System.Drawing.Size(837, 264);
             this.dgvChuaHP.TabIndex = 0;
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "STT";
-            this.Column5.Name = "Column5";
-            this.Column5.ReadOnly = true;
-            this.Column5.Width = 50;
-            // 
-            // Column1
-            // 
-            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column1.HeaderText = "Mã Số Sinh Viên";
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            // 
-            // Column2
-            // 
-            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column2.HeaderText = "Số tiền đăng ký";
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
-            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            // 
-            // Column3
-            // 
-            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column3.HeaderText = "Số tiền phải đóng";
-            this.Column3.Name = "Column3";
-            this.Column3.ReadOnly = true;
-            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            // 
-            // Column4
-            // 
-            this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.Column4.HeaderText = "Số tiền còn lại";
-            this.Column4.Name = "Column4";
-            this.Column4.ReadOnly = true;
-            this.Column4.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvChuaHP.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvChuaHP_CellContentClick);
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Controls.Add(this.btnThoat);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 340);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 419);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(27, 0, 0, 0);
             this.flowLayoutPanel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(628, 44);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(837, 54);
             this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // cboHocKy
+            // 
+            this.cboHocKy.FormattingEnabled = true;
+            this.cboHocKy.Location = new System.Drawing.Point(317, 22);
+            this.cboHocKy.Name = "cboHocKy";
+            this.cboHocKy.Size = new System.Drawing.Size(318, 32);
+            this.cboHocKy.TabIndex = 4;
+            this.cboHocKy.DropDownClosed += new System.EventHandler(this.cboHocKy_DropDownClosed);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Học kỳ:";
             // 
             // ChuaHTHP
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(628, 384);
+            this.ClientSize = new System.Drawing.Size(837, 473);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.grBoxThongTin);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "ChuaHTHP";
             this.Text = "DSSV chưa hoàn thành HP";
+            this.Load += new System.EventHandler(this.ChuaHTHP_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.grBoxThongTin.ResumeLayout(false);
@@ -273,19 +181,11 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label title;
         private System.Windows.Forms.GroupBox grBoxThongTin;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox_HocKy;
-        private System.Windows.Forms.ComboBox comboBoxNamHoc;
         private System.Windows.Forms.Button btnThoat;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.DataGridView dgvChuaHP;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.Button btnTruyVan;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ComboBox cboHocKy;
+        private System.Windows.Forms.Label label3;
     }
 }
--- DangKyHocPhan/ChuaHTHP.cs
+++ DangKyHocPhan/ChuaHTHP.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Globalization;
 
 namespace DangKyHocPhan
 {
@@ -38,6 +40,48 @@ namespace DangKyHocPhan
         }
 
         private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ChuaHTHP_Load(object sender, EventArgs e)
+        {
+            // Load cboHocKy
+            string query = "SELECT * FROM dbo.DSHOCKY ORDER BY NamHoc DESC, HocKy DESC";
+            using (SqlDataAdapter adapter = new SqlDataAdapter(query, new SqlConnection(Properties.Settings.Default.DKHPConnectionString)))
+            {
+                DataTable data = new DataTable();
+                adapter.Fill(data);
+                data.Columns.Add("HienThi", typeof(String));
+
+                foreach (DataRow row in data.Rows)
+                {
+                    row["HienThi"] = ("Học kỳ " + (row["HocKy"].ToString() == "0" ? "hè" : row["HocKy"].ToString()) + " - Năm học " + row["NamHoc"].ToString());
+                }
+
+                cboHocKy.DataSource = data;
+                cboHocKy.ValueMember = "MaHK";
+                cboHocKy.DisplayMember = "HienThi";
+            }
+        }
+
+        private void cboHocKy_DropDownClosed(object sender, EventArgs e)
+        {
+            string query = "select PHIEUDK.SoPhieu, sum(THUHOCPHI.SoTienThu) as [TienDaDong], Sum(CEILING(SoTiet * 1.0 / SoTinChi) * GiaTien) as [TienPhaiDong] from PHIEUDK full join THUHOCPHI on THUHOCPHI.SoPhieu = PHIEUDK.SoPhieu join DKHOCPHAN on DKHOCPHAN.SoPhieu = PHIEUDK.SoPhieu join MONHOC on MONHOC.MaMon = DKHOCPHAN.MonHoc join LOAIMON on LOAIMON.MaLoaiMon = MONHOC.LoaiMon where MaHK = @MaHK group by PHIEUDK.SoPhieu having(isnull(sum(THUHOCPHI.SoTienThu),0) < Sum(CEILING(SoTiet * 1.0 / SoTinChi) * GiaTien))";
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.DKHPConnectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@MaHK", cboHocKy.SelectedValue.ToString());
+                DataTable dataTable = new DataTable();
+                dataTable = new DataTable();
+                connection.Open();
+                dataTable.Load(command.ExecuteReader());
+                connection.Close();
+                dgvChuaHP.DataSource = dataTable;
+            }
+        }
+
+        private void dgvChuaHP_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
